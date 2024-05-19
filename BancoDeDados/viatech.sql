ee3caa68d89fa1d05d26af60ec39d4e9a611d7b1
Samarah Silva <samarah.silva@jazztech.corp>
2024-05-19 18:13:01 -0300

    Scrpit e der criados

--- BancoDeDados/viatech.sql
+++ BancoDeDados/viatech.sql
@@ -1,66 +1,104 @@
+DROP DATABASE viatech;
 CREATE DATABASE viatech;
 USE viatech;
 
+
 CREATE TABLE empresa(
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nomeFantasia VARCHAR(100) NOT NULL,
-CNPJ CHAR(14) NOT NULL,
-telefone CHAR(11) NOT NULL,
-email VARCHAR(100) NOT NULL,
-senha VARCHAR(100) NOT NULL
+razaoSocial VARCHAR(100) NOT NULL,
+apelido VARCHAR (100) NULL,
+CNPJ CHAR(14) NOT NULL
 );
 
 CREATE TABLE funcionario(
-idFuncionario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-fkEmpresa INT NOT NULL,
-CONSTRAINT fkEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+urlFoto VARCHAR(300) NULL,
 nome VARCHAR(100) NOT NULL,
-cargo VARCHAR(100) NOT NULL,
+cpf CHAR(11) NOT NULL,
 email VARCHAR(100) NOT NULL,
-senha VARCHAR(100) NOT NULL
+senha VARCHAR(100) NOT NULL,
+cargo VARCHAR(100) NOT NULL,
+fkEmpresaFun INT NOT NULL,
+CONSTRAINT fkEmpresaFun FOREIGN KEY (fkEmpresaFun) REFERENCES empresa (idEmpresa)
 );
 
 CREATE TABLE linha(
 idLinha INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-fkEmpresa INT NOT NULL,
-CONSTRAINT fkEmpresaLinha FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
 nome VARCHAR(100) NOT NULL,
-numero INT NOT NULL
+numero INT NOT NULL,
+fkEmpresaLinha INT NOT NULL,
+CONSTRAINT fkEmpresaLinha FOREIGN KEY (fkEmpresaLinha) REFERENCES empresa (idEmpresa)
 );
 
 CREATE TABLE estacao(
 idEstacao INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nome VARCHAR(100) NOT NULL,
 fkLinha INT NOT NULL,
-CONSTRAINT fkLinha FOREIGN KEY (fkLinha) REFERENCES linha (idLinha),
-nome VARCHAR(100)
+CONSTRAINT fkLinha FOREIGN KEY (fkLinha) REFERENCES linha (idLinha)
 );
 
-CREATE TABLE maquinaMonitorada (
-idMaquina INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE computador (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+dominio VARCHAR(50) NOT NULL,
+ip VARCHAR(50) NOT NULL,
+sistemaOperacional VARCHAR(45) NOT NULL,
 fkEstacao INT NOT NULL,
 CONSTRAINT fkEstacao FOREIGN KEY (fkEstacao) REFERENCES estacao (idEstacao)
 );
 
-CREATE TABLE disco (
-idDisco INT PRIMARY KEY AUTO_INCREMENT,
-fkMaquina INT NOT NULL,
-CONSTRAINT fkMaquina FOREIGN KEY (fkMaquina) REFERENCES maquinaMonitorada (idMaquina),
-velocidadeLeitura VARCHAR (40) NOT NULL,
-espacoTotal VARCHAR (40) NOT NULL,
-espacoDisponivel VARCHAR (40) NOT NULL
+
+CREATE TABLE registroCpu (
+  idCpu INT PRIMARY KEY AUTO_INCREMENT,
+  dtHora DATETIME NOT NULL,
+  porcentagemUso VARCHAR(45) NOT NULL,
+  temperatura DOUBLE NOT NULL
 );
 
-CREATE TABLE ram (
+CREATE TABLE registroDisco (
+  idDisco INT PRIMARY KEY AUTO_INCREMENT,
+  dtHora DATETIME NOT NULL,
+  gigabyteLeitura DOUBLE NOT NULL,
+  gigabyteEscrita DOUBLE NOT NULL,
+  usoArmazenamento DOUBLE
+);
+
+CREATE TABLE registroUsb(
+idUsb INT PRIMARY KEY AUTO_INCREMENT,
+dtHora DATETIME,
+nomeDispositivo VARCHAR(100)
+);
+
+CREATE TABLE registroRam(
 idRam INT PRIMARY KEY AUTO_INCREMENT,
-memoriaDisponivel VARCHAR (40),
-memoriaTotal VARCHAR(40),
-memoriaUtilizada VARCHAR (40)
+dtHora DATETIME,
+memoriaUtilizada VARCHAR(45)
 );
 
+
+CREATE TABLE componentes(
+idComponente INT PRIMARY KEY AUTO_INCREMENT,
+nomeCpu  VARCHAR(255),
+frequenciaCpu VARCHAR(50),
+qtdCpuFisica VARCHAR(45),
+qtdCpuLogica VARCHAR(45),
+capacidadeTotalArmazenamento DOUBLE,
+memoriaRamTotal VARCHAR(45),
+fkRegistroCpu INT,
+fkRegistroDisco INT,
+fkRegistroRam INT,
+fkRegistroUsb INT,
+CONSTRAINT fkRegistroCpu FOREIGN KEY (fkRegistroCpu) REFERENCES registroCpu (idCpu),
+CONSTRAINT fkRegistroDisco FOREIGN KEY (fkRegistroDisco) REFERENCES registroDisco (idDisco),
+CONSTRAINT fkRegistroRam FOREIGN KEY (fkRegistroRam) REFERENCES registroRam (idRam),
+CONSTRAINT fkRegistroUsb FOREIGN KEY (fkRegistroUsb) REFERENCES registroUsb (idUsb)
+);
+
+
+-- NÃO EXECUTEI ESSA TABELA
 CREATE TABLE processador (
 idCpu INT PRIMARY KEY AUTO_INCREMENT,
 fkMaquina INT NOT NULL,
-CONSTRAINT FOREIGN KEY (fkMaquina) REFERENCES maquinaMonitorada (idMaquina),
+CONSTRAINT FOREIGN KEY (fkMaquina) REFERENCES computador (idMaquina),
 nomeCPU VARCHAR (255),
 frequencia VARCHAR (40),
 threads VARCHAR (40),
