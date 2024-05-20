3fe9b1db8d2b0281094b2d489ab3abbad244d80a
EduardoPulcino <eduardo.pulcino@sptech.school>
2024-05-19 22:19:49 -0300

    Adiciona DER e script BD

--- BancoDeDados/viatech.sql
+++ BancoDeDados/viatech.sql
@@ -1,12 +1,11 @@
-DROP DATABASE viatech;
 CREATE DATABASE viatech;
 USE viatech;
 
-
 CREATE TABLE empresa(
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-razaoSocial VARCHAR(100) NOT NULL,
-apelido VARCHAR (100) NULL,
+razaoSocial VARCHAR(245) NOT NULL,
+nomeFantasia VARCHAR(245),
+apelido VARCHAR (245),
 CNPJ CHAR(14) NOT NULL
 );
 
@@ -18,16 +17,16 @@ cpf CHAR(11) NOT NULL,
 email VARCHAR(100) NOT NULL,
 senha VARCHAR(100) NOT NULL,
 cargo VARCHAR(100) NOT NULL,
-fkEmpresaFun INT NOT NULL,
-CONSTRAINT fkEmpresaFun FOREIGN KEY (fkEmpresaFun) REFERENCES empresa (idEmpresa)
+fkEmpresa INT NOT NULL,
+CONSTRAINT FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
 );
 
 CREATE TABLE linha(
 idLinha INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nome VARCHAR(100) NOT NULL,
 numero INT NOT NULL,
-fkEmpresaLinha INT NOT NULL,
-CONSTRAINT fkEmpresaLinha FOREIGN KEY (fkEmpresaLinha) REFERENCES empresa (idEmpresa)
+fkEmpresa INT NOT NULL,
+CONSTRAINT FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
 );
 
 CREATE TABLE estacao(
@@ -37,8 +36,8 @@ fkLinha INT NOT NULL,
 CONSTRAINT fkLinha FOREIGN KEY (fkLinha) REFERENCES linha (idLinha)
 );
 
-CREATE TABLE computador (
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE maquina (
+idMaquina INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 dominio VARCHAR(50) NOT NULL,
 ip VARCHAR(50) NOT NULL,
 sistemaOperacional VARCHAR(45) NOT NULL,
@@ -47,63 +46,35 @@ CONSTRAINT fkEstacao FOREIGN KEY (fkEstacao) REFERENCES estacao (idEstacao)
 );
 
 
-CREATE TABLE registroCpu (
-  idCpu INT PRIMARY KEY AUTO_INCREMENT,
-  dtHora DATETIME NOT NULL,
-  porcentagemUso VARCHAR(45) NOT NULL,
-  temperatura DOUBLE NOT NULL
-);
-
-CREATE TABLE registroDisco (
-  idDisco INT PRIMARY KEY AUTO_INCREMENT,
-  dtHora DATETIME NOT NULL,
-  gigabyteLeitura DOUBLE NOT NULL,
-  gigabyteEscrita DOUBLE NOT NULL,
-  usoArmazenamento DOUBLE
-);
-
-CREATE TABLE registroUsb(
-idUsb INT PRIMARY KEY AUTO_INCREMENT,
-dtHora DATETIME,
-nomeDispositivo VARCHAR(100)
-);
-
-CREATE TABLE registroRam(
-idRam INT PRIMARY KEY AUTO_INCREMENT,
-dtHora DATETIME,
-memoriaUtilizada VARCHAR(45)
-);
-
-
-CREATE TABLE componentes(
-idComponente INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE especificacaoMaquina(
+idEspecificacaoMaquina INT PRIMARY KEY AUTO_INCREMENT,
 nomeCpu  VARCHAR(255),
 frequenciaCpu VARCHAR(50),
 qtdCpuFisica VARCHAR(45),
 qtdCpuLogica VARCHAR(45),
 capacidadeTotalArmazenamento DOUBLE,
-memoriaRamTotal VARCHAR(45),
-fkRegistroCpu INT,
-fkRegistroDisco INT,
-fkRegistroRam INT,
-fkRegistroUsb INT,
-CONSTRAINT fkRegistroCpu FOREIGN KEY (fkRegistroCpu) REFERENCES registroCpu (idCpu),
-CONSTRAINT fkRegistroDisco FOREIGN KEY (fkRegistroDisco) REFERENCES registroDisco (idDisco),
-CONSTRAINT fkRegistroRam FOREIGN KEY (fkRegistroRam) REFERENCES registroRam (idRam),
-CONSTRAINT fkRegistroUsb FOREIGN KEY (fkRegistroUsb) REFERENCES registroUsb (idUsb)
+ramTotal DOUBLE,
+fkMaquina INT,
+CONSTRAINT FOREIGN KEY (fkMaquina) REFERENCES maquina (idMaquina)
 );
 
-
--- NÃO EXECUTEI ESSA TABELA
-CREATE TABLE processador (
-idCpu INT PRIMARY KEY AUTO_INCREMENT,
-fkMaquina INT NOT NULL,
-CONSTRAINT FOREIGN KEY (fkMaquina) REFERENCES computador (idMaquina),
-nomeCPU VARCHAR (255),
-frequencia VARCHAR (40),
-threads VARCHAR (40),
-tempoAtividade VARCHAR (255)
+CREATE TABLE registro (
+  idRegistro INT PRIMARY KEY AUTO_INCREMENT,
+  dtHora DATETIME NOT NULL,
+  discoGigabyteLeitura DOUBLE NOT NULL,
+  discoGigabyteEscrita DOUBLE NOT NULL,
+  cpuPorcentagemUso DOUBLE,
+  cpuTemperatura DOUBLE,
+  nomeDispositivo VARCHAR(100),
+  ramUtilizada DOUBLE,
+  usbNomeDispositivos VARCHAR(245)  ,
+  fkEspecificacaoMaquina INT,
+  CONSTRAINT FOREIGN KEY (fkEspecificacaoMaquina) REFERENCES especificacaoMaquina (idEspecificacaoMaquina)
 );
 
-
-SELECT * FROM ram;
+INSERT INTO empresa VALUES
+	(null, "ViaMobilidade LTDA", "Viaiznha", "12345678911234");
+    
+INSERT INTO funcionario  VALUES
+	(null, null, "Teste", "12345678901", "teste@teste.com", "123#Ee", "gerencia", 1);
+    
