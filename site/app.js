058c04f7159b33bcc6f9bf4faeb20044fc3fc642
Renan Alves <renan.rocha@sptech.school>
2024-05-24 19:35:46 -0300

    configurações sql server no web data viz

--- site/app.js
+++ site/app.js
@@ -1,9 +1,18 @@
-process.env.AMBIENTE_PROCESSO = "desenvolvimento";
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
+// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+
+require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3000 : 8080;
+var PORTA_APP = process.env.APP_80;
+var HOST_APP = process.env.APP_341974486;
+
 
 var app = express();
 
@@ -25,7 +34,17 @@ app.use("/dashboard", dashboardRouter);
 
 app.listen(PORTA, function () {
     console.log(`
-    Servidor do seu site já está rodando!
-    http://localhost:${PORTA}/html/index.html 
-    Você está se conectando ao banco LOCAL (MySQL Workbench).`);
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    \n\n\n                                                                                                 
+    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
+    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
+    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
+    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
+    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
--- site/src/database/config.js
+++ site/src/database/config.js
@@ -1,38 +1,38 @@
 var mysql = require("mysql2");
 
+// CONEXÃO DO BANCO MYSQL SERVER
 var mySqlConfig = {
-    host: "localhost",
-    database: "Viatech",
-    user: "root",
-    password: "rabello",
+    host: process.env.DB_341974486,
+    database: process.env.DB_viatech,
+    user: process.env.DB_sa,
+    password: process.env.DB_urubu100,
+    port: process.env.DB_80
 };
 
 function executar(instrucao) {
 
-    if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        return new Promise(function (resolve, reject) {
-            var conexao = mysql.createConnection(mySqlConfig);
-            conexao.connect();
-            conexao.query(instrucao, function (erro, resultados) {
-                conexao.end();
-                if (erro) {
-                    reject(erro);
-                }
-                console.log(resultados);
-                resolve(resultados);
-            });
-            conexao.on('error', function (erro) {
-                return ("ERRO NO MySQL WORKBENCH: ", erro.sqlMessage);
-            });
-        });
-    } else {
-        return new Promise(function (resolve, reject) {
-            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-            reject("AMBIENTE NÃO CONFIGURADO EM app.js")
-        });
+    if (process.env.AMBIENTE_PROCESSO !== "producao" && process.env.AMBIENTE_PROCESSO !== "desenvolvimento") {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM .env OU dev.env OU app.js\n");
+        return Promise.reject("AMBIENTE NÃO CONFIGURADO EM .env");
     }
+
+    return new Promise(function (resolve, reject) {
+        var conexao = mysql.createConnection(mySqlConfig);
+        conexao.connect();
+        conexao.query(instrucao, function (erro, resultados) {
+            conexao.end();
+            if (erro) {
+                reject(erro);
+            }
+            console.log(resultados);
+            resolve(resultados);
+        });
+        conexao.on('error', function (erro) {
+            return ("ERRO NO MySQL SERVER: ", erro.sqlMessage);
+        });
+    });
 }
 
 module.exports = {
     executar
-}
+};
